e833321b6cd8dff1aac62d45571475a7552a31a9
Ben W <mrbrwells@gmail.com>
2016-05-30 13:59:02 -0400

    fluid layout with mobile sidebar

--- public/custom.js
+++ public/custom.js
@@ -1,7 +1,28 @@
+function doItToIt() {
+  console.log('fire');
+  // $(this).transition('vertical flip', '500ms').hide()//.transition('vertical flip', '600ms')
+  // $('.itembenn').transition('vertical flip', '500ms')//.transition('vertical flip', '600ms')
+    // .on('mouseenter', doItToIt)
+}
+
+function leave() {
+  $(this)
+    .transition('vertical flip in').on('mouseenter', doItToIt)
+}
+
 (function($) {
   setTimeout(function() {
-    $('.ui.sidebar')
-    // .addClass('visible');
-    .transition('fly up');
+    // $('.ui.sidebar')
+    // // .addClass('visible');
+    // .transition('fly up');
+    // $('.item').on(function(e) {
+    //   console.log('hover');
+    //   $(this).transition('vertical flip in').css('background', 'red');
+    // },function(e) {
+    //   $(this).transition('horizontal flip in').css('background', 'none');
+    // });
+    console.log('setTimeout');
+    $('.itemben').on('mouseenter', doItToIt);
+    // $('.item').on('mouseleave', leave);
   }, 500)
 })(jQuery);
